3f95c61a88f21d1940f5b2067bf6ab52a4608e22
Lucas <lucasjlima2006@gmail.com>
2025-09-28 17:26:46 -0300

    functions and logic created

--- Exercicio-2-Calcula-Idade/Exercicio-2-Calcula-Idade/Program.cs
+++ Exercicio-2-Calcula-Idade/Exercicio-2-Calcula-Idade/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+public struct Pessoa
+{
+    public string NomeCompleto;
+    public DateTime DataNascimento;
+
+
+    public int CalcularIdade()
+    {
+        DateTime dataAtual = DateTime.Today;
+        int idade = dataAtual.Year - DataNascimento.Year;
+
+        if (DataNascimento.Date > dataAtual.AddYears(-idade))
+        {
+            idade--;
+        }
+
+        return idade;
+    }
+
+    public bool EhMaiorDeIdade()
+    {
+        return CalcularIdade() >= 18;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa usuario = new Pessoa();
+
+            Console.WriteLine("Digite seu nome completo:");
+            usuario.NomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Digite sua data de nascimento (formato dd/MM/yyyy):");
+            string dataInput = Console.ReadLine();
+            usuario.DataNascimento = DateTime.ParseExact(dataInput, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            int idadeCalculada = usuario.CalcularIdade();
+
+            Console.WriteLine($"\nOlá, {usuario.NomeCompleto}.");
+            Console.WriteLine($"Sua idade atual é: {idadeCalculada} anos.");
+
+            if (usuario.EhMaiorDeIdade())
+            {
+                Console.WriteLine("Você é maior de idade.");
+                Console.WriteLine("Você já pode iniciar o processo para tirar sua carteira de habilitação.");
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade.");
+                Console.WriteLine("Você ainda não pode iniciar o processo para tirar a carteira de habilitação.");
+            }
+        }
+    }
+}
